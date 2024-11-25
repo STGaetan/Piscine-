d7c1965bf0abb040f474c20cd61d9f492c1046ce
STGaetan <guitare.spagnulo@gmail.com>
2024-11-25 10:51:42 +0100

    Création fonction pou ajout et liste

--- piscine-debut.py
+++ piscine-debut.py
@@ -1,21 +1,28 @@
 liste = [("Pierre","Dos",10),("Paul","Brasse",13),("Léa","Crawl",6), ("Léa","Brasse",8) ]
 commande = ''
-
-
 isAlive = True
+
+def cmd_ajout(liste):
+    """Ajoute un évenement à la liste"""
+    a = input("Qui nage ? ")
+    b = input("quelle nage ? ")
+    c = input("combien de longueur ? ")
+    liste.append((a,b,c))
+
+def cmd_liste(liste):
+    """Affiche tous les performances des nageurs"""
+    for elt in liste:
+        print(f"Prénom : {elt[0]}, Type de nage : {elt[1]}, Combien de longueur : {elt[2]}")
+
 while isAlive:
     commande = input("Que faut-il faire ? ")
 
     if commande == 'ajout':
-        a = input("Qui nage ? ")
-        b = input("quelle nage ? ")
-        c = input("combien de longueur ? ")
-        liste.append((a,b,c))
+        cmd_ajout(liste)
         continue
    
     if commande == 'liste':
-        for elt in liste:
-            print(f"Prénom {elt[0]}, nage {elt[1]}, longueur {elt[2]}")
+        cmd_liste(liste)
         continue
 
     if commande == "exit":
