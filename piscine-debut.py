8bad4f0189c44acc95a76444bdc49be87a06df52
STGaetan <guitare.spagnulo@gmail.com>
2024-12-17 16:03:18 +0100

    clarification du code

--- piscine-debut.py
+++ piscine-debut.py
@@ -1,5 +1,5 @@
 liste = [(1,3,10),(2,1,13),(3,2,6), (3,1,8) ]
-listeNageur = [(1, "Pierre"), (2, "Paul"), (3, "Léa")]
+listeNageurs = [(1, "Pierre"), (2, "Paul"), (3, "Léa")]
 listeNages = [(1, 'Brasse'), (2, 'Crawl'), (3, "dos")]
 commande = ''
 
@@ -8,6 +8,7 @@ def get_str_from_num_in_list(num, liste):
     for elt in liste:
         if elt[0]==num:
             return elt[1]
+    #la ligne suivante ne devrait jamais être exécutée
     return "unknown"
 
 def cmd_individu(listeNageur):
@@ -26,7 +27,7 @@ def cmd_nouvellenage(listeNages):
 
 def cmd_ajout(liste):
     """Ajoute un évenement à la liste"""
-    for elt in listeNageur:
+    for elt in listeNageurs:
         print(f"{elt[0]:5} : {elt[1]}")
     a = int(input("Nageur n° ? "))
     for elt in listeNages:
@@ -40,14 +41,14 @@ def cmd_liste(liste):
     print("Prénom       |   nage    |   longueur")
     print("-------------------------------------")
     for elt in liste:
-        nageur = get_str_from_num_in_list(elt[0], listeNageur)
+        nageur = get_str_from_num_in_list(elt[0], listeNageurs)
         nage = get_str_from_num_in_list(elt[1], listeNages)
         print(f" {nageur:11}| {nage:8}|  {elt[2]}")
 
 
 def cmd_nageur(liste):
     """Affiche tableau d'un nageur"""
-    for elt in listeNageur:
+    for elt in listeNageurs:
         print(f"{elt[0]:5} : {elt[1]}")
     tmp = int(input("Quel numéro de nageur ? "))
     print("Performances de ", tmp)
@@ -89,6 +90,10 @@ def cmd_load(liste, filename):
         liste.append(tuple(tmp))
     fichier.close()
 
+#
+#   Programme principal
+#
+
 def cmd_exit():
     """Fini la boucle"""
     temp= input('En êtes-vous sûr ? (o)ui/(n)on ?')
@@ -107,7 +112,7 @@ while isAlive:
         continue
 
     if commande == 'individu':
-        cmd_individu(listeNageur)
+        cmd_individu(listeNageurs)
         continue
     
     if commande == 'nouvelle nage':
